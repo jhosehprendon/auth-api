4b52469a4e6d7fb05d31c552301a6386da10598f
Jhosehp <jhosehp.rendon@gmail.com>
2018-03-10 17:23:27 -0500

    Changed dirname

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ console.log('Server listening on: ', port);
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "index.html"))
+    res.sendFile(path.resolve(__dirname, client))
 });
 
 
